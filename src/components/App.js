cdf0eb6393f1ea8c34d03b7d11a695cb123d2d1b
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-07-29 20:47:49 +0100

    Test Complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
-// create your App component here
+import React, {useState, useEffect} from "react";
+
+
+function App(){
+    const [image, setimage] = useState('');
+    const [loading, setloading] = useState(false)
+
+    useEffect(()=>{
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(Response=>Response.json())
+        .then(data=>{
+            setimage((img)=>img=data.message);
+            setloading((load)=>!load);
+        })
+    }, [])
+
+    if (!loading) return <p>Loading...</p>
+
+    return(
+        <div>
+            <img src={image} alt="A Random Dog"/>
+        </div>
+    );
+}
+
+
+
+
+
+export default App;
